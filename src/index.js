37fb505a120a614685479ad80bd991cbf1d62cfc
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-27 23:01:58 -0400

    general styles and framework

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import './styles.scss';
+
+const d = document;
+
+const body = d.querySelector('body');
+const navbar = d.createElement('nav');
+const header = d.createElement('header');
+const main = d.createElement('main');
+const footer = d.createElement('footer');
+
+body.appendChild(navbar);
+body.appendChild(header);
+body.appendChild(main);
+body.appendChild(footer);
+
